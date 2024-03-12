b44fe17ed5588633da0f7ff62d02426539f50216
Arvan Yudhistia Ardana <arvanardana1@gmail.com>
2024-03-13 04:40:17 +0700

    feat: Booking card component

---
+++ app/src/main/java/com/arvan/xplorein/ui/component/CardComponent.kt
@@ -0,0 +1,120 @@
+package com.arvan.xplorein.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.arvan.xplorein.ui.theme.orange
+import com.arvan.xplorein.ui.theme.yellow
+
+@Composable
+fun BookingCard(
+    date: String,
+    price: String,
+    onSeeDetailsClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .background(yellow, RoundedCornerShape(16.dp))
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Penida Island",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = date,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Divider(color = Color.Black, thickness = 1.dp, modifier = Modifier.padding(vertical = 8.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Harga: $price",
+                modifier = Modifier.weight(1f)
+            )
+            Button(
+                onClick = { onSeeDetailsClick() }
+            ) {
+                Text(text = "See Details")
+            }
+        }
+    }
+}
+
+@Composable
+fun ElevatedCardExample( date: String,
+                         price: String,
+                         onSeeDetailsClick: () -> Unit
+
+) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp
+        ),
+        modifier = Modifier
+            .size(width = 550.dp, height = 170.dp)
+            .fillMaxWidth()
+        , colors = CardDefaults.elevatedCardColors(containerColor = yellow)
+
+    ) {
+        Column(
+            modifier = Modifier
+                .background(yellow, RoundedCornerShape(16.dp))
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Penida Island",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = date,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Divider(color = orange, thickness = 2.dp, modifier = Modifier.padding(vertical = 8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = " $price",
+                    modifier = Modifier.weight(1f),
+                    fontWeight = FontWeight.Bold
+                )
+                Button(
+                    onClick = { onSeeDetailsClick() },
+                    colors = ButtonDefaults.buttonColors(containerColor = orange)
+                ) {
+
+                    Text(text = "See Details")
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/arvan/xplorein/ui/presentation/booking/BookingPage.kt
+++ app/src/main/java/com/arvan/xplorein/ui/presentation/booking/BookingPage.kt
@@ -1,30 +1,131 @@
 package com.arvan.xplorein.ui.presentation.booking
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.arvan.xplorein.common.BottomNavBar
+import com.arvan.xplorein.ui.component.ElevatedCardExample
+import com.arvan.xplorein.ui.theme.booking
+import com.arvan.xplorein.ui.theme.orange
+import com.arvan.xplorein.ui.theme.yellow
+import kotlinx.coroutines.launch
+
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun BookingScreen(navController: NavController) {
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Booking",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+            },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = booking)
+        )
+    }) { innerPadding -> // Use padding from the Scaffold
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding) // Apply Scaffold's padding
+        ) {
+            val pagerState = rememberPagerState(pageCount = {2})
+            val tabTitles = listOf("Places", "Guides")
+            val coroutineScope = rememberCoroutineScope()
+
+            // Add height to TabRow
+            TabRow(
+                selectedTabIndex = pagerState.currentPage,
+                containerColor = Color.White,
+                contentColor = Color.Black,
+                divider = ({  }),
+                indicator = { tabPositions ->
+                    val tabWidth = (tabPositions[pagerState.currentPage].right - tabPositions[pagerState.currentPage].left) / 1f // Adjust calculation as needed
+                    TabRowDefaults.Indicator(
+                        modifier = Modifier
+                            .tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                            .padding(horizontal = 48.dp)
+                            .clip(CircleShape),
+                        height = 10.dp,
+                        color = orange
+                    )
+                },
+                modifier = Modifier.height(48.dp) // Add height to the TabRow
+            ) {
+                tabTitles.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = pagerState.currentPage == index,
+                        onClick = { coroutineScope.launch { pagerState.animateScrollToPage(index) } }
+                    )
+                }
+            }
+
+            HorizontalPager(
+                state = pagerState,
+               // Allow pager to occupy remaining space
+            ) { page ->
+                when (page) {
+                    0 -> PlacesPage()
+                    1 -> GuidesPage()
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun BookingScreen(
-    navController: NavController
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Booking Screen", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+fun GuidesPage() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Guides Page",
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun PlacesPage() {
+    Column( modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+        ) {
+      ElevatedCardExample(date = "17 September 2024", price = "Rp. 28.590", onSeeDetailsClick = {})
     }
 }
